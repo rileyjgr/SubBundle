2f002788e47603ad990c9688aa597894bd319116
Riley Griffin <rigriffin241@gmail.com>
2019-02-16 19:03:34 -0600

    cleaned up code and added more to categories functionallity

--- controllers/bot-hook.js
+++ controllers/bot-hook.js
@@ -1,23 +1,62 @@
 const {google} = require("googleapis");
+const mongoose = require('mongoose');
 const axios = require('axios');
 
 require('dotenv').config();
 
 module.exports = {
-    budget: async(agent)=>{
-    
+
+    // check whether user has enough money to buy a certain product
+    afford: async(agent)=>{
+
     },
-    test: async(agent)=>{
-        agent.add('test worked.');
-    },
-    test2: async(agent)=>{
-        agent.add('test worked.');
-    },
+
+    // check balance on given query
     balance: async(agent)=>{
+        const array = 
+            {
+            'name': 'Riley',
+            'totalBalance': 1000,
+            'food': 100,
+            'gas': 20,
+            'Subscriptions': 100,
+            'misc':20
+            };
+
+        const query = agent.parameters.categories;
+        console.log(query);
+
+        // switch case to handle each category
+        switch(query){
+            case 'food':
+                agent.add('Hello, ' +array.name + '.' + ' Your total remaining balance for food is: ' + array.food + ' dollars');
+                break;
+            case 'misc': 
+                agent.add('Hello, ' +array.name + '.' + 'Your total remaining balance for everything else is: ' + array.misc + ' dollars');
+                break;
+            case 'gas':
+                agent.add('Hello, ' +array.name + '.' + 'Your total remaining balance for gas is: ' + array.gas + ' dollars');
+                break;
+            case 'total':
+                agent.add('Hello, ' +array.name + '.' + ' Your total remaining balance is: ' + array.totalBalance + ' dollars');
+                break;
+
+            case 'subscriptions':
+                agent.add('Hello, ' +array.name + '.' + ' Your total remaining balance for subscriptions is: ' + array.Subscriptions + ' dollars');
+                break;
+            case '':
+                agent.add(array.name + '. Please ask about a certain category. Your choices include subscriptions, gas, food, misc, and total balance');
+            }
 
     },
     payments: async(agent)=>{
-        
+
+    },
+    spent: async(agent)=>{
+
+    },
+    salary: async(agent)=>{
+
     }
 };
 
--- controllers/botcontroller.js
+++ controllers/botcontroller.js
@@ -9,11 +9,15 @@ require('dotenv').config();
 exports.dialogflowFirebaseFulFillment = functions.https.onRequest((request, response)=>{
     const agent = new WebhookClient({request, response});
     let intentMap = new Map();
+
+    // intent mapping
     
-    intentMap.set('test', bot.test);
-    intentMap.set('test2', bot.test2);
+    intentMap.set('afford', bot.salary);
     intentMap.set('balance', bot.balance);
+    intentMap.set('spent', bot.salary);
+    intentMap.set('salary', bot.salary);
     intentMap.set('upcommingPayments', bot.payments);
+
     agent.handleRequest(intentMap);
 
 });
